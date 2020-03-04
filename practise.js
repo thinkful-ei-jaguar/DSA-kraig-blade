af1b707119a13318bc59e29894980aeb2d5a5cfd
KRAIG WILLIAMS <williamskrg@gmail.com>
2020-03-04 12:02:34 -0500

    step 2 complete

---
+++ practise.js
@@ -0,0 +1,17 @@
+const MyArray = require('./MyArray') ;
+
+
+function main(){
+
+    MyArray.SIZE_RATIO = 3;
+
+    // Create an instance of the Array class
+    let arr = new Array();
+
+    // Add an item to the array
+    arr.push(3);
+
+    console.log(arr);
+}
+
+main()
